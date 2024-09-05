57996b435235167adc298b6979aa9aaa8222865a
hata0 <haataa0100@gmail.com>
2024-09-05 23:10:23 +0900

    feat: CardLoadingUI を作成

--- Assets/Project/InGame/Program/Scripts/Turn/Turn.cs
+++ Assets/Project/InGame/Program/Scripts/Turn/Turn.cs
@@ -1,6 +1,8 @@
 ﻿#nullable enable
 
 using Cysharp.Threading.Tasks;
+using R3;
+using R3.Triggers;
 using System.Threading;
 using UnityEngine;
 
@@ -12,6 +14,8 @@ namespace InGame
         private Player _opponentPlayer = null!;
         private int _maxTurn;
         private int _turn = 1;
+        private bool _isCardLoading;
+        [SerializeField] private GameObject _cardLoadingUI = null!;
 
         public void Initialize(Player player, Player opponentPlayer, int maxTurn)
         {
@@ -21,6 +25,20 @@ namespace InGame
 
             CancellationToken ct = this.GetCancellationTokenOnDestroy();
             HandleTurn(ct).Forget();
+
+            this.UpdateAsObservable().Subscribe(_ => UpdateUI()).AddTo(this);
+        }
+
+        private void UpdateUI()
+        {
+            if (_isCardLoading)
+            {
+                _cardLoadingUI.SetActive(true);
+            }
+            else
+            {
+                _cardLoadingUI.SetActive(false);
+            }
         }
 
         private async UniTaskVoid HandleTurn(CancellationToken ct)
@@ -28,14 +46,17 @@ namespace InGame
             while (_turn <= _maxTurn)
             {
                 // TODO: 実際にはカードを読み込み終わるまで待つ
+                _isCardLoading = true;
                 Debug.Log($"turn: {_turn}");
                 Debug.Log("実際にはカードを読み込み終わるまで待つ");
                 _player.SetCurrentCard(new(CardHand.Paper, CardType.Fire, 400));
                 _opponentPlayer.SetCurrentCard(new(CardHand.Scissors, CardType.Grass, 1000));
                 await UniTask.DelayFrame(120, PlayerLoopTiming.FixedUpdate, ct);
 
+                _isCardLoading = false;
                 CompareCard();
                 _turn++;
+                await UniTask.DelayFrame(120, PlayerLoopTiming.FixedUpdate, ct);
             }
         }
 

12cf468cb2d7d0a9bb51982cacf7d17ba25c8db6
hata0 <haataa0100@gmail.com>
2024-09-06 18:32:36 +0900

    feat: QRCodeReader を作成

---
+++ Assets/Project/Common/Program/Scripts/QRCode/QRCodeReader.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+
+using UnityEngine;
+using ZXing;
+
+namespace Common
+{
+    public class QRCodeReader
+    {
+        public string ReadQRCodeTexture(Texture2D tex)
+        {
+            BarcodeReader reader = new BarcodeReader();
+            // 読み込み
+            Result r = reader.Decode(tex.GetPixels32(), tex.width, tex.height);
+
+            return r != null ? r.Text : string.Empty;
+        }
+    }
+}

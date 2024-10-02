d543e12958659cb892bd3f819d4e4a83da3a802e
hata0 <haataa0100@gmail.com>
2024-10-02 16:22:15 +0900

    feat: UnityDebugSheet を導入

---
+++ Assets/Project/InGame/Program/Scripts/DebugSheet/InGameDebugPage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityDebugSheet.Runtime.Core.Scripts;
+using UnityEngine;
+
+public sealed class InGameDebugPage : DefaultDebugPageBase
+{
+    protected override string Title { get; } = "Example Debug Page";
+
+    public override IEnumerator Initialize()
+    {
+        // Add a button to this page.
+        AddButton("Example Button", clicked: () => { Debug.Log("Clicked"); });
+
+        yield break;
+    }
+}
---
+++ Assets/Project/InGame/Program/Scripts/DebugSheet/InGameDebugSheetController.cs
@@ -0,0 +1,14 @@
+﻿using UnityDebugSheet.Runtime.Core.Scripts;
+using UnityEngine;
+
+public sealed class InGameDebugSheetController : MonoBehaviour
+{
+    private void Start()
+    {
+        // Get or create the root page.
+        var rootPage = DebugSheet.Instance.GetOrCreateInitialPage();
+
+        // Add a link transition to the ExampleDebugPage.
+        rootPage.AddPageLinkButton<InGameDebugPage>(nameof(InGameDebugPage));
+    }
+}

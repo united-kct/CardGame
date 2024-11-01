6d8c348c6de6040013c2d6c7fa45e02de0f1f263
pyaabot <anonym527xx@gmail.com>
2024-11-01 18:44:01 +0900

    タイトル画面作成

--- Assets/Project/Title/Program/Scripts/UIAnimation/Blink.cs
+++ Assets/Project/Title/Program/Scripts/UIAnimation/Blink.cs
@@ -3,17 +3,31 @@ using System.Threading;
 using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Project.Common.Program.Scripts.UIAnimation
 {
     public class Fade : MonoBehaviour
     {
+        public float speed = 0.5f;
         [SerializeField] private TextMeshProUGUI text;
-        private float _alpha = 0;
+        [SerializeField] private RawImage img;
+        private float time;
 
         private void FixedUpdate()
         {
-            text.alpha = _alpha;
+            text.color = GetAlphaColor(text.color);
+            img.color = GetAlphaColor(img.color);
+        }
+
+        Color GetAlphaColor(Color color)
+        {
+            time += Time.deltaTime * 3.0f * speed;
+            color.a = Mathf.Sin(time);
+
+            return color;
         }
     }
 }
